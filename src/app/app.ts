331b4ac84d52ec7e05ebd036e041df8bc0a6f63a
Pavan Gawade <gawadepavanmca@gmail.com>
2025-06-25 13:44:32 +0530

    string program

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'StringOperation';
+
+  firstString: string = '';
+  secondString: string = '';
+  upperCaseString : string = '';
+  lowerCaseString: string = '';
+  lengthString: string = '';
+  sliceString : string = '';
+  sentenceString: string = '';
+
+  getLongestWord(sentence: string): string {
+  if (!sentence) return '';
+  return sentence
+    .split(' ')
+    .reduce((longest, current) => current.length > longest.length ? current : longest, '');
+}
 }
